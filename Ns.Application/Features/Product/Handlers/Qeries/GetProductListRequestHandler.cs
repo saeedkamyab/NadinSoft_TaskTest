53d51ceeae665d3aec69e23412ce48ed3ed66193
saeedkamyab <saeed.kamyab@yahoo.com>
2024-04-09 03:12:28 -0700

    I made request handler to handel the request

---
+++ Ns.Application/Features/Product/Handlers/Qeries/GetProductListRequestHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Ns.Application.DTOs.Product;
+using Ns.Application.Features.Product.Requests.Queries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ns.Application.Features.Product.Handlers.Qeries
+{
+    public class GetProductListRequestHandler : IRequestHandler<GetProductListRequest, List<ProductListDto>>
+    {
+        public Task<List<ProductListDto>> Handle(GetProductListRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
