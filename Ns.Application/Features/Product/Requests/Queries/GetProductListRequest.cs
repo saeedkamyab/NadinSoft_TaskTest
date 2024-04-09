9954c026b773c4475878dd5129c164a3a0b1d8e0
saeedkamyab <saeed.kamyab@yahoo.com>
2024-04-09 03:06:37 -0700

    I installed MediatR to use cqrs and i made my request class

---
+++ Ns.Application/Features/Product/Requests/Queries/GetProductListRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Ns.Application.DTOs.Product;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Ns.Application.Features.Product.Requests.Queries
+{
+    public class GetProductListRequest:IRequest<List<ProductListDto>>
+    {
+
+    }
+}
