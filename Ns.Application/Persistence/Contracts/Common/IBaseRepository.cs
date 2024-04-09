86e0da26531a4ad3fdb4adb274a1110ebece650c
saeedkamyab <saeed.kamyab@yahoo.com>
2024-04-09 01:56:23 -0700

    made application layer and baserepository interface and product repository

---
+++ Ns.Application/Persistence/Contracts/Common/IBaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ns.Application.Persistence.Contracts.Common
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> Get(int id);
+
+        Task<IReadOnlyList<TEntity>> GetAll();
+
+        Task<TEntity> Add(TEntity entity);
+
+        Task<TEntity> Update(TEntity entity);
+
+        Task<TEntity> Delete(int id);
+
+    }
+}
---
+++ Ns.Application/Persistence/Contracts/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ns.Application.Persistence.Contracts.Common;
+using Ns.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ns.Application.Persistence.Contracts
+{
+    public interface ProductRepository:IBaseRepository<Product>
+    {
+
+    }
+}
