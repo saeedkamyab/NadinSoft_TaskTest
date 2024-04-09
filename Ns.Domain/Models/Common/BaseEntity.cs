c6c8c7fafa3369a4e81da275125e8413c9a869b9
saeedkamyab <saeed.kamyab@yahoo.com>
2024-04-09 01:36:38 -0700

    I made Domain and Product entity model

---
+++ Ns.Domain/Models/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ns.Domain.Models.Common
+{
+    public abstract class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+}
---
+++ Ns.Domain/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using Ns.Domain.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ns.Domain.Models
+{
+    public class Product : BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public DateTime ProduceDate { get; set; }
+        public string ManufacturePhone { get; set; }
+        public string ManufactureEmail { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
