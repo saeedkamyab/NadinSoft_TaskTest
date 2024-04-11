be7135d7e24a60582ec78e0380b3c5681a2c314f
saeedkamyab <saeed.kamyab@yahoo.com>
2024-04-11 07:39:38 -0700

    auth service

--- Ns.Identity/Services/AuthService.cs
+++ Ns.Identity/Services/AuthService.cs
@@ -1,5 +1,8 @@
-﻿using Ns.Application.Contracts.Identity;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Ns.Application.Contracts.Identity;
 using Ns.Application.Models.Identity;
+using Ns.Identity.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +13,17 @@ namespace Ns.Identity.Services
 {
     public class AuthService : IAuthService
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IOptions<JwtSetings> _jwtSettings;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AuthService(UserManager<ApplicationUser> userManager
+            , IOptions<JwtSetings> jwtSettings, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _jwtSettings = jwtSettings; 
+            _signInManager = signInManager;
+        }
         public Task<AuthResponse> Login(AuthRequest loginRequest)
         {
             throw new NotImplementedException();
